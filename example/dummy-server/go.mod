e4878224abb6709a2dcd725c1d26154101398173
u2386 <hugo.cavan2386@gmail.com>
2021-10-04 18:35:31 +0800

    :tada: add example

---
+++ example/dummy-server/go.mod
@@ -0,0 +1,12 @@
+module example.com/dummy-server
+
+go 1.16
+
+require (
+	github.com/drone/signal v1.0.0
+	github.com/go-delve/delve v1.7.2 // indirect
+	github.com/gorilla/mux v1.8.0
+	github.com/mitchellh/mapstructure v1.4.2 // indirect
+	github.com/u2386/go-hijack v0.0.0-20211004103111-13d124e20c9a
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+)
---
+++ example/dummy-server/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/drone/signal"
+	"github.com/gorilla/mux"
+	gohijack "github.com/u2386/go-hijack"
+)
+
+
+//go:noinline
+func DoingSomething() {
+}
+
+func EchoHandler(w http.ResponseWriter, r *http.Request) {
+	body, _ := io.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	DoingSomething()
+	fmt.Fprintf(w, "echo:%s", body)
+}
+
+func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	if err := gohijack.Hijack(ctx); err != nil {
+		panic(err)
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/echo", EchoHandler)
+	http.Handle("/", r)
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:8000",
+	}
+	go func() {
+		fmt.Fprintf(os.Stderr, "serving on:%s\n", srv.Addr)
+		fmt.Fprintf(os.Stderr, "server exits:%s\n", srv.ListenAndServe())
+	}()
+	signal.WithContextFunc(ctx, func() { srv.Shutdown(ctx); cancel() })
+	<-ctx.Done()
+	time.Sleep(3 * time.Second)
+}
